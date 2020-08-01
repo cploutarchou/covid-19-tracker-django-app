0ff6b7b829beb569635c9612e102dd3ce3bd680d
Christos Ploutarchou <cploutarchou@gmail.com>
2020-08-01 23:29:50 +0300

    update settings.py

--- app/settings.py
+++ app/settings.py
@@ -55,8 +55,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'coronavirus/templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'coronavirus/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
