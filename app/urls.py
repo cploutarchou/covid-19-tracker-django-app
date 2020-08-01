dd4c0c6269c5c1cda1dd0bedce23fa9c30457337
Christos Ploutarchou <cploutarchou@gmail.com>
2020-08-02 00:49:56 +0300

    update urls.py

--- app/urls.py
+++ app/urls.py
@@ -17,8 +17,27 @@ from django.contrib import admin
 from django.urls import path, include
 from coronavirus import views
 
+from django.contrib import admin
+from django.conf.urls import url
+from django.urls import path, include
+
+from django.views.generic import TemplateView
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+# Loading plotly Dash apps script
+import coronavirus.dash_app_code
+
+# from django_plotly_dash.views import add_to_session
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('django_plotly_dash/', include('django_plotly_dash.urls')),
+    # NO needed
+    # path('', include('django.contrib.auth.urls')),
+    url('^dash_plot$', TemplateView.as_view(template_name='dash_plot.html'), name="dash_plot"),
+    url('^django_plotly_dash/', include('django_plotly_dash.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
