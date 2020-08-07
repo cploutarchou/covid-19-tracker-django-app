733ddc1701c28a1dd16bfbb79c08b5de5fd13d67
Christos Ploutarchou <cploutarchou@gmail.com>
2020-08-07 10:39:17 +0300

    corrections

--- app/manage_sources.py
+++ app/manage_sources.py
@@ -1,10 +1,13 @@
 import os
-stats_files = list()
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))+"/covid"
+base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+directory = os.path.join(base_dir, "sources")
 
-directory = os.path.join(BASE_DIR, "sources")
-for root, dirs, files in os.walk(directory):
-    for file in files:
-        if file.endswith(".csv"):
-            stats_files.append(file)
+
+def get_files():
+    stats_files = list()
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith(".csv"):
+                stats_files.append(f"{directory}/{file}")
+    return stats_files
