6766136d380dc8a84b2b41b18a769be8f20dc253
Christos Ploutarchou <cploutarchou@gmail.com>
2020-08-09 22:12:30 +0300

    create DailyStats model class.

--- coronavirus/models.py
+++ coronavirus/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class DailyStats(models.Model):
+    date = models.DateField()
+    new_cases = models.IntegerField(max_length=50)
+    deaths = models.IntegerField(max_length=50)
+    daily_tests = models.IntegerField(max_length=50)
+    last_updated = models.DateTimeField()
+    updated = models.BooleanField(default=False)
