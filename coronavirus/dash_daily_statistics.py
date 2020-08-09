390b4bfac88d40df4d761091471bab72116f7594
Christos Ploutarchou <cploutarchou@gmail.com>
2020-08-09 21:47:49 +0300

    create first dash element for dashboard

--- coronavirus/dash_daily_statistics.py
+++ coronavirus/dash_daily_statistics.py
@@ -3,45 +3,79 @@ import dash_html_components as html
 import pandas as pd
 from django_plotly_dash import DjangoDash
 from app.manage_sources import get_files
-from datetime import datetime
-
+from datetime import datetime as d
+import plotly.graph_objs as go
 
 files = get_files()
 
 
 def date_parse(x):
-    return datetime.strptime(x, '%d/%m/%y')
+    return d.strptime(x, '%d/%m/%Y')
 
-
-idx = pd.date_range('2020-08-1', periods=30, freq='D')
 
 csv_file = None
 for file in files:
     if "cy_covid_19_daily_stats" in file:
-        print("file found")
         csv_file = file
 
-
-df = pd.read_csv(csv_file,
-                 encoding="UTF-8")
+df = pd.read_csv(
+    csv_file,
+    parse_dates=['date'],
+    date_parser=date_parse,
+    infer_datetime_format=True
+)
 
 df['date'] = df['date'].astype('datetime64[ns]')
-df['total tests'] = pd.to_numeric(df['total tests'], errors='coerce')
-# df['total tests'] = df['total tests'].astype('int64')
-# df.fillna(0)
+df['day'] = pd.DatetimeIndex(df['date']).day
 
+df['month'] = pd.DatetimeIndex(df['date']).month
+df['year'] = pd.DatetimeIndex(df['date']).year
+df['total tests'] = pd.to_numeric(df['total tests'], errors='coerce')
+df['total tests'] = df['total tests'].fillna(0)
+df['total tests'] = df['total tests'].astype('int64')
+df['daily tests performed'] = pd.to_numeric(df['daily tests performed'], errors='coerce')
+
+df = df.fillna(0)
+df['daily tests performed'] = df['daily tests performed'].astype('int64')
+df['daily deaths'] = pd.to_numeric(df['daily deaths'], errors='coerce')
+
+df['daily deaths'] = df['daily deaths'].astype('int64')
+daily_cases_df = df[[
+    'daily new cases',
+    'daily deaths',
+    'daily tests performed',
+    'month',
+    'day',
+    'year'
+]]
+
+today = d.today()
+current_date = d(today.year, today.month, 1)
+month = current_date.month
+year = current_date.year
+filtered_df = daily_cases_df[daily_cases_df['month'] == 8]
 app = DjangoDash('dash_daily_statistics')
+fig = go.Figure(data=[go.Scatter(x=filtered_df['day'], y=filtered_df['daily new cases'])])
+fig.update_layout(
+    title={
+        'text': "COVID-19 Cyprus Daily Statistics",
+        'y': 0.9,
+        'x': 0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'},
+    xaxis={
+        'title': 'Dates'
+    },
+    yaxis={
+        'title': 'Daily New Cases'
+    }
+
+)
 
 app.layout = html.Div([
+
     dcc.Graph(
         id='bar-chart',
-        figure={
-            'data': [
-                {'x': df['date'], 'y': df['daily new cases'], 'type': 'bar', 'name': 'CY DAILY'}
-            ],
-            'layout': {
-                'title': 'Current month daily stats'
-            }
-        }
-    )
-], className='col')
+        figure=fig
+
+    )])
