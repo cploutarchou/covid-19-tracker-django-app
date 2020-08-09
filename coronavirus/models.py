db11ddf46a4f241c1ee515b247169968f2af114e
Christos Ploutarchou <cploutarchou@gmail.com>
2020-08-09 22:39:47 +0300

    create JobsLogs class

--- coronavirus/models.py
+++ coronavirus/models.py
@@ -1,4 +1,16 @@
+import datetime
+
 from django.db import models
+
+
+class JobsLogs(models.Model):
+    id = models.AutoField(primary_key=True)
+    started_at = models.DateTimeField(default=datetime.datetime.now, editable=False)
+    completed_at = models.DateTimeField()
+    task_names = models.CharField(max_length=100)
+    data = models.TextField()
+    status = models.CharField(max_length=100)
+    executed = models.BooleanField(default=False)
 
 
 class DailyStats(models.Model):
