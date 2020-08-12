611ac10ef60fe7817472b2a3c7bd6ed2791f2fc1
Christos Ploutarchou <cploutarchou@gmail.com>
2020-08-12 22:51:17 +0300

    correnction

--- coronavirus/dash_daily_statistics.py
+++ coronavirus/dash_daily_statistics.py
@@ -6,7 +6,6 @@ from app.manage_sources import get_files
 from datetime import datetime as d
 import plotly.graph_objs as go
 
-
 from coronavirus.models import DailyStats
 
 files = get_files()
@@ -59,9 +58,6 @@ for index, row in df.iterrows():
     )
     if res:
         continue
-
-
-
 
 today = d.today()
 current_date = d(today.year, today.month, 1)
