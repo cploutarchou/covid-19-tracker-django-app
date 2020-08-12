8187c853253b0632abee2faeedc76d43feaffc9b
Christos Ploutarchou <cploutarchou@gmail.com>
2020-08-13 00:45:32 +0300

    update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,3 @@
-
 """app URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -31,11 +30,21 @@ import coronavirus.dash_daily_statistics
 
 from django_plotly_dash.views import add_to_session
 
+from admin_panel import views as admin_panel_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('django_plotly_dash/', include('django_plotly_dash.urls')),
-    path('', include('django.contrib.auth.urls')),
     url('^django_plotly_dash/', include('django_plotly_dash.urls')),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', TemplateView.as_view(template_name='admin_base.html'), name='home'),
+    path('secure_admin/', admin_panel_views.IndexView.as_view(), name='index'),
+    path('secure_admin/<int:pk>/', admin_panel_views.ContactDetailView.as_view(), name='detail'),
+    path('secure_admin/edit/<int:pk>/', admin_panel_views.edit, name='edit'),
+    path('secure_admin/create/', admin_panel_views.create, name='create'),
+    path('secure_admints/delete/<int:pk>/', admin_panel_views.delete, name='delete')
+
+]
+urlpatterns += [
+    path('secure_admin/', include('django.contrib.auth.urls')),
 ]
