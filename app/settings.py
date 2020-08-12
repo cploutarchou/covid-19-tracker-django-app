09b476dcaa9ecaf441b02f7c494b5721842b85d7
Christos Ploutarchou <cploutarchou@gmail.com>
2020-08-12 23:31:19 +0300

    add new app admin_panel template folder to settings

--- app/settings.py
+++ app/settings.py
@@ -59,7 +59,10 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'coronavirus/templates/default')],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'coronavirus/templates/default'),
+            os.path.join(BASE_DIR, 'admin_panel/templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
