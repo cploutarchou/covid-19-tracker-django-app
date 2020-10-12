53f73ec70fcbff4be30c2ca77f695b5e676b939f
Christos Ploutarchou <cploutarchou@gmail.com>
2020-10-12 21:32:41 +0300

    correction

--- app/views.py
+++ app/views.py
@@ -1,19 +1,17 @@
 # Create your views here.
-from django.http import HttpResponse
 from django.shortcuts import render
-from django.template import loader
 
 
 def index(request):
-    return render(request, "index.html")
+    return render(request, 'index.html')
 
 
 def pages(request):
     context = {}
-    try:
-        load_template = request.path.split('/')[-1]
-        template = loader.get_template('pages/' + load_template)
-        return HttpResponse(template.render(context, request))
-    except:
-        template = loader.get_template('pages/404.html')
-        return HttpResponse(template.render(context, request))
+#     try:
+#         load_template = request.path.split('/')[-1]
+#         template = loader.get_template('pages/' + load_template)
+#         return HttpResponse(template.render(context, request))
+#     except:
+#         template = loader.get_template('pages/404.html')
+#         return HttpResponse(template.render(context, request))
