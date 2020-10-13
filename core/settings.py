6bd6bf05648a85d3f5d0f81cfc1898bd9200ab08
Christos Ploutarchou <cploutarchou@gmail.com>
2020-10-13 23:20:46 +0300

    update configuration settings

--- core/settings.py
+++ core/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = config('SECRET_KEY', default='S#kokoskokou')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
