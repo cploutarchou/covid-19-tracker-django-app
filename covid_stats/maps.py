e63e2d1ce838e85ecca92342b897b96bfb9e2d8b
Christos Ploutarchou <cploutarchou@gmail.com>
2020-10-15 23:35:50 +0300

    create function cyprus_map

--- covid_stats/maps.py
+++ covid_stats/maps.py
@@ -1,2 +1,10 @@
+import json
+
+from urllib.request import urlopen
+
+
 def cyprus_map():
-    pass
+    with urlopen(
+            'https://raw.githubusercontent.com/cploutarchou/covid-19-tracker-django-app/master/data-sources/cyprus'
+            '.geojson') as response:
+        cyprus = json.load(response)
