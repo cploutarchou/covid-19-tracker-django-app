e8704f91f783c85c7e8091ee5e97c2c693f89664
Christos Ploutarcgou <cploutarchou@gmail.com>
2020-10-19 21:52:45 +0300

    update settings.py

--- core/settings.py
+++ core/settings.py
@@ -99,9 +99,9 @@ USE_TZ = True
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'core')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'core/static'),
+    # os.path.join(BASE_DIR, 'core/static'),
 )
