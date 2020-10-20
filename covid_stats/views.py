aad50bdba3075eabca7a7b516b29dfebf80288f3
Christos Ploutarcgou <cploutarchou@gmail.com>
2020-10-20 22:25:30 +0300

    update dashboard data

--- covid_stats/views.py
+++ covid_stats/views.py
@@ -1,18 +1,8 @@
 from django.shortcuts import render
 
-from . import maps, data
-
-
-def index(request):
-    return render(request, template_name='index.html')
+from . import maps
 
 
 def maps_page(request):
     plot_div = maps.cyprus_map()
     return render(request, template_name='pages/maps.html', context={'cyprus_map': plot_div})
-
-
-def new_cases(request):
-    cases = data.daily_new_cases()
-    _data = {"title": "Today Cases", "cases": cases}
-    return render(request=request, template_name="cards/new_cases.html", context=_data)
