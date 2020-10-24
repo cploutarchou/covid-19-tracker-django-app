16cdb19bf20fd325cd8f511bdfc6cb1cae26e947
Christos Ploutarcgou <cploutarchou@gmail.com>
2020-10-24 19:53:42 +0300

    create functions get_yesterday_date and  get_two_dates_before

--- core/functions.py
+++ core/functions.py
@@ -1,6 +1,23 @@
+import datetime
+
+
 def percentage_difference_calculator(old_value, new_value):
     val1 = float(old_value)
     val2 = float(new_value)
     rate = (((val1 - val2) / ((val1 + val2) / 2)) * 100)
     rate = -rate
-    return format(rate, ".2f")
+    if rate is not None:
+        return format(rate, ".2f")
+    else:
+        return False
+
+
+def get_yesterday_date():
+    yesterday = datetime.date.today() - datetime.timedelta(days=2)
+    yesterday = yesterday.strftime('%Y-%m-%d')
+    return yesterday
+
+
+def get_two_dates_before():
+    res = datetime.date.today() - datetime.timedelta(days=3)
+    res = res.strftime('%Y-%m-%d')
