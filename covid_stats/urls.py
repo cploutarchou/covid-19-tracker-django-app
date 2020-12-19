ac233f768da04fd6ec4b48d7d5328d465951233d
Christos Ploutarchou <cploutarchou@gmail.com>
2020-12-19 11:39:52 +0200

    Update covid_stats app urls

--- covid_stats/urls.py
+++ covid_stats/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('maps.html', views.maps_page, name='maps'),
+    path('pages/rabid_test_stats.html', views.rabid_tests_stats, name='rabid_test_stats'),
 ]
