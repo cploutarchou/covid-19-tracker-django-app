376c870bc6b7905405c11709616308258a71eee8
Christos Ploutarchou <cploutarchou@gmail.com>
2020-12-19 11:38:53 +0200

    Create rabid_tests_stats function on views

--- covid_stats/views.py
+++ covid_stats/views.py
@@ -6,3 +6,8 @@ from . import maps
 def maps_page(request):
     plot_div = maps.cyprus_map()
     return render(request, template_name='pages/maps.html', context={'cyprus_map': plot_div})
+
+
+def rabid_tests_stats(request):
+    index_page_context = {}
+    return render(request, template_name="pages/rabid_test_stats.html", context=index_page_context)
